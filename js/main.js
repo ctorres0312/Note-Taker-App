afa35ce452f80e326f9a4c581bd0ccfbf136248f
Christian <cktorres0312@gmail.com>
2023-05-24 15:43:43 -0400

    v1 work in progress

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import NotesAPI from './NotesAPI.js'
+
+
+NotesAPI.deleteNote(833587);
+
+
+console.log(NotesAPI.getAllNotes());
+
+
+
+
+
+// .saveNote({
+//     id: 833587,
+//     title: 'New Note!',
+//     body: 'water.'
+
+// });
